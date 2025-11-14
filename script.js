148dc88f47583254142c8ed7987d291ebecdd17a
unw10181 <uchennanwilliams@gmail.com>
2025-11-13 23:19:12 -0500

    p1

---
+++ script.js
@@ -0,0 +1,10 @@
+const form = document.getElementById("registrationForm");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const confirmPassword = document.getElementById("confirmPassword");
+
+const usernameError = document.getElementById("usernameError");
+const emailError = document.getElementById("emailError");
+const passwordError = document.getElementById("passwordError");
+const confirmPasswordError = document.getElementById("confirmPasswordError");
