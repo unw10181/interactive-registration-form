31b10e39ff71e3ea35b01a42208e770d32860b9e
unw10181 <uchennanwilliams@gmail.com>
2025-11-13 23:22:30 -0500

    p2

--- script.js
+++ script.js
@@ -8,3 +8,16 @@ const usernameError = document.getElementById("usernameError");
 const emailError = document.getElementById("emailError");
 const passwordError = document.getElementById("passwordError");
 const confirmPasswordError = document.getElementById("confirmPasswordError");
+//Each form part selected
+
+//Checks DOM for if the username has been stored already.
+document.addEventListener("DOMContentLoaded", () => {
+  const saved = localStorage.getItem("savedUsername");
+  if (saved) username.value = saved;
+});
+
+//Event listeners for when each of these elements 
+username.addEventListener("input", validateUsername);
+email.addEventListener("input", validateEmail);
+password.addEventListener("input", validatePassword);
+confirmPassword.addEventListener("input", validateConfirmPassword);
